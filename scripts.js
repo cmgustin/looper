5bd5d4b8729ca5cf39bcb147b4141f9d5acbf549
Chris Gustin <gustin.chris@gmail.com>
2022-11-01 19:58:47 -0400

    More awesomeness + beating head against wall on progress indicator

--- scripts.js
+++ scripts.js
@@ -5,9 +5,20 @@ const bpmInput = document.getElementById("bpmInput");
 const tracks = [];
 const players = [];
 const loops = [];
-let metronomeLoop;
 
 // Functions
+function play() {
+  Tone.Transport.start()
+}
+
+function pausePlayback() {
+  Tone.Transport.pause()
+}
+
+function stopPlayback() {
+  Tone.Transport.stop()
+}
+
 function updateBPM(bpmValue) {
   Tone.Transport.bpm.value = bpmValue;
 }
@@ -19,12 +30,29 @@ function startRecording(event) {
   const mic = new Tone.UserMedia().connect(recorder);
 
   const button = event.target
-
+  const trackId = event.target.dataset.id
+  const inputSource = document.querySelector(`#track-${trackId} .selectInputDevice`).value;
+  const numberOfBars = document.querySelector(`#track-${trackId} .numberOfBars`).value;
+  const numberOfBarsConverted = Tone.Time(numberOfBars * 2).toBarsBeatsSixteenths()
+  const progressBar = document.querySelector(`#track-${trackId} .progress`)
 
   mic
-    .open()
+    .open(inputSource)
     .then(() => {
       recorder.start();
+
+      Tone.Transport.scheduleRepeat(time => {
+        const timeArray = Tone.Time(time).toBarsBeatsSixteenths().split(":");
+        const bars = timeArray[0]
+        const beats = timeArray[1]
+
+        if (bars % numberOfBars == 0 && beats == 0) {
+          progressBar.innerHTML = "[]"
+        } else {
+          progressBar.innerHTML += "[]"
+        }
+
+      }, "0:1:0")
 
       Tone.Transport.schedule((time) => {
         recorder.stop().then((blob) => {
@@ -32,12 +60,12 @@ function startRecording(event) {
           const player = new Tone.Player(url).toDestination();
           const loop = new Tone.Loop((time) => {
             player.start(time);
-          }, "1:0:0").start(Tone.now());
+          }, numberOfBarsConverted).start(Tone.now());
 
           players.push(player);
           loops.push(loop);
         });
-      }, "1:0:0");
+      }, numberOfBarsConverted);
 
       Tone.Transport.start();
     })
@@ -46,7 +74,7 @@ function startRecording(event) {
     });
 }
 
-function loadInputs(select) {
+function loadAvailableInputs(select) {
   Tone.UserMedia.enumerateDevices().then((devices) => {
     const html = devices.map((device) => {
         return `<option value="${device.deviceId}">${device.label}</option>`;
@@ -59,7 +87,7 @@ function loadInputs(select) {
 function listAvailableInputs() {
   document.querySelectorAll(".selectInputDevice").forEach(select => {
     if (!select.classList.contains("initialized")) {
-      loadInputs(select);
+      loadAvailableInputs(select);
       select.classList.add("initialized");
     }
   })
@@ -76,7 +104,7 @@ function getTrackHTML() {
     </select>
     <label>Bars</label>
     <input type="number" value="1" class="numberOfBars">
-    <progress value="0" max="100"></progress>
+    <span class="progress"></span>
   `;
   return track
 }
@@ -89,9 +117,9 @@ function createNewTrack() {
 }
 
 function startMetronome() {
-  metronomeLoop = new Tone.Loop(() => {
+  Tone.Transport.scheduleRepeat(() => {
     metronome.classList.toggle("on")
-  }, "0:1:0").start(0)
+  }, "4n")
 }
 
 function initialize() {
